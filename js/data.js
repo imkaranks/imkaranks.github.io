db3605301f4c9956df381e9baa9a86bf1c618f4b
Karan Sethi <109339437+imkaranks@users.noreply.github.com>
2022-12-04 04:03:25 +0530

    Add files via upload
    Added JS files

---
+++ js/data.js
@@ -0,0 +1,50 @@
+export const services = [
+    {
+        id: 1,
+        title: "responsive",
+        icon: "fa-tablet",
+        content: "Gone those days when there was no term 'responsiveness', with the rise of too many modern devices responsiveness is must.",
+    },
+    {
+        id: 2,
+        title: "new technology",
+        icon: "fa-code",
+        content: "I always prefer to work with new and stable frameworks to provide clients better and modernize experience.",
+    },
+    {
+        id: 3,
+        title: "ui/ux",
+        icon: "fa-search",
+        content: "I make sure that user interface and experience are pleasant and less complex without compromising the aesthetics.",
+    },
+];
+export const projects = [
+    {
+        title: "apna college",
+        text: "Tried to clone Apna College website, the UI is quite fascinating and I want know if I could clone it or not. Result came quite better than my expectation.",
+        url: "/case-studies/apna-college.html",
+        isNew: true,
+        isComplete: true,
+    },
+    {
+        title: "panic modal",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam vero deleniti doloremque iste possimus aliquid dolor itaque. Impedit, in nostrum?",
+        url: "#",
+        isNew: true,
+        isComplete: false,
+    },
+    {
+        title: "carousel",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam vero deleniti doloremque iste possimus aliquid dolor itaque. Impedit, in nostrum?",
+        url: "#",
+        isNew: true,
+        isComplete: false,
+    },
+    {
+        title: "pagination",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam vero deleniti doloremque iste possimus aliquid dolor itaque. Impedit, in nostrum?",
+        url: "#",
+        isNew: true,
+        isComplete: false,
+    },
+];
---
+++ js/index.js
@@ -0,0 +1,67 @@
+const primaryNav = document.getElementById("primary-nav");
+const navToggle = document.getElementById("nav-toggle");
+const serviceWrapper = document.getElementById("service__wrapper");
+const themeToggle = document.getElementById("theme-toggle");
+/* added event listeners */
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute("data-visible") ?
+        navToggle.setAttribute("aria-expanded", false) :
+        navToggle.setAttribute("aria-expanded", true);
+
+    primaryNav.toggleAttribute("data-visible");
+});
+
+const replaceClass = (element, prevClass, newClass)=>{
+    element.classList.replace(prevClass, newClass)
+}
+
+themeToggle.addEventListener('click',()=>{
+    const body = document.body;
+    const themeIcon = document.getElementById("theme-icon");
+    if(body.classList.contains("light")) {
+        replaceClass(body, "light", "dark");
+        replaceClass(themeIcon, "fa-sun-o", "fa-moon-o");
+    }
+    else {
+        replaceClass(body, "dark", "light");
+        replaceClass(themeIcon, "fa-moon-o", "fa-sun-o");
+    }
+});
+
+import { services as services } from "./data.js";
+import { projects as projects } from "./data.js";
+
+services.forEach((item) => {
+    /* construct card content */
+    const content = `
+    <div class="service__card shadow-hard relative overflow-hidden p-4 bg-background rounded-sm flex" aria-labelledby="service__card-title">
+        <div class="service__card-icon relative text-2xl">
+            <i class="fa ${item.icon}" aria-hidden="true"></i>
+        </div>
+        <div class="service__card-body relative flow" style="--flow-space: .5em;">
+            <h3 class="service__card-title relative font-heading text-2xl uppercase">${item.title}</h3>
+            <p class="service__card-text relative">
+                ${item.content}
+            </p>
+        </div>`;
+    serviceWrapper.innerHTML += content;
+});
+
+const projectWrapper = document.querySelector("#project-wrapper")
+
+projects.forEach(item => {
+    const content = `
+    <div class="project-item flow bg-surface p-4" ${!item.isComplete?'title="To be developed"':''} style="--flow-space: 0.5em;">
+        <div class="flex items-center" style="--gap: .25em;">
+            <div class="project-icon bg-accent-500 text-neutral-100 flex items-center justify-center">
+                <i class="fas fa-code"></i>
+            </div>
+            <h3 class="text-xl text-heading capitalize">${item.title}</h3>
+        </div>
+        <p class="text-sm text-regular">
+            ${item.text}
+        </p>
+        <a href=${item.url} class="block">View Live <i class="fas fa-arrow-right"></i></a>
+    </div>`;
+    projectWrapper.innerHTML += content;
+});
