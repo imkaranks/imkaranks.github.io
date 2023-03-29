dab6ec737657f055b2e5ac920910274f93d8a305
Karan Sethi <109339437+imkaranks@users.noreply.github.com>
2023-03-29 18:54:11 +0530

    Update app.js
    Minor changes

--- projects/spotify/js/app.js
+++ projects/spotify/js/app.js
@@ -1,6 +1,9 @@
 const $secondaryNav = document.querySelector(".secondary-nav");
 const $btnMenu = document.querySelector(".btn-menu")
-const $songContainers = document.querySelectorAll(".song-container");
+const $playlistContainers = document.querySelectorAll(".grid-playlist");
+const $primaryNavLinks = document.querySelectorAll('[ data-require-login="true"]');
+const $modal = document.querySelector(".modal");
+const $closeModalBtn = document.querySelector(".btn-close-modal");
 
 $btnMenu.addEventListener('click', () => {
   const isExpanded = $secondaryNav.getAttribute("data-expanded");
@@ -14,56 +17,6 @@ $btnMenu.addEventListener('click', () => {
     $btnMenu.setAttribute("aria-expanded", false);
   }
 });
-
-function shuffle(array) {
-  const newArray = [...array];
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-  }
-
-  return newArray;
-}
-
-function randomize(arr) {
-  let i, j, tmp;
-  for (i = arr.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      tmp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = tmp;
-  }
-  return arr;
-}
-
-const playlist = [
-  "Peaceful Piano",
-  "Deep Focus",
-  "Instrumental Study",
-  "Jazz Vibes",
-  "Focus Flow",
-  "Today's Top Hits",
-  "RapCaviar",
-  "All Out 2010s",
-  "Rock Classics",
-  "Chill Hits",
-];
-
-for (let i = 0; i < 10; i++) {
-  $songContainers.forEach(container => {
-    const shuffledPlaylist = shuffle(playlist);
-    const shuffledIndex = shuffle([1,2,3,4,5,6,7,8,9,10]);
-    container.innerHTML += `
-      <div class="song-card">
-        <img src="./assets/thumbnail (${shuffledIndex[i]}).jpg" alt="song-thumbnail">
-        <h3>${shuffledPlaylist[i]}</h3>
-        <p>Lorem ipsum dolor sit amet consectetur.</p>
-        <div class="song-play-icon">
-          <i class="fa-solid fa-play"></i>
-        </div>
-      </div>`;
-  });
-}
 
 const $prevBtns = document.querySelectorAll('.btn-prev');
 const $nextBtns = document.querySelectorAll('.btn-next');
@@ -89,3 +42,17 @@ function slideNext(container) {
 function slidePrev(container) {
   container.scrollLeft -= 190;
 }
+
+$primaryNavLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    if($modal.style.display !== "block"){
+      $modal.style.display = "block";
+    }
+    $modal.style.top = `${event.pageY}px`;
+    $modal.style.transform = "translateY(-20%)";
+  })
+});
+
+$closeModalBtn.addEventListener('click', () => {
+  $modal.style.display = "none";
+})
