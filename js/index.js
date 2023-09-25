712dbe6a427a1e5127b67c1dec2815e9a8ccfc3a
Karan <risingphoenix160@gmail.com>
2023-09-26 00:02:51 +0530

    style: minor tweaks

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-import portfolio from "./data/portfolio.js";
-import services from "./data/services.js";
+"use strict";
 
-(function(){
+(function () {
   const $heroHeadline = document.querySelector(".hero__headline");
   const $headerContent = document.querySelector(".header__content");
   const $themeBtn = document.querySelector(".btn--theme");
@@ -10,8 +9,31 @@ import services from "./data/services.js";
   const $portfolioContent = document.querySelector(".portfolio-cards");
   const $primaryNav = document.querySelector(".primary-nav");
   const $navItems = document.querySelectorAll(".primary-nav-list .nav__item");
+  const $cursor = document.getElementById('cursor');
 
   let prev = 0;
+
+  if (window.matchMedia('(pointer: fine)').matches) {
+    $cursor.style.opacity = '1';
+    document.onmousemove = function (event) {
+      if (
+        event.target.tagName === 'A' ||
+        event.target.tagName === 'BUTTON' ||
+        event.target.parentNode.tagName === 'A' ||
+        event.target.parentNode.tagName === 'BUTTON'
+      ) {
+        $cursor.classList.add('link');
+        $cursor.textContent = 'Click';
+      } else {
+        $cursor.classList.remove('link');
+        $cursor.textContent = '';
+      }
+      gsap.to($cursor, {
+        top: event.pageY + 'px',
+        left: event.pageX + 'px'
+      });
+    }
+  }
 
   window.onload = function () {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -19,18 +41,29 @@ import services from "./data/services.js";
       document.body.classList.add('dark');
     }
     animateHeadline(['creative', 'designing', 'development', 'crafting'], $heroHeadline);
+    fetch('./data.json')
+      .then(raw => raw.json())
+      .then(data => {
+        const { portfolio, services } = data;
+        renderSection($portfolioContent, portfolio);
+        renderSection($servicesContent, services);
+      })
+      .catch(error => console.log(error));
   }
 
   window.onscroll = function () {
-    const styles = $headerContent.style;
     if (scrollY > 75) {
-      styles.position = "fixed";
-      styles.top = "0";
-      styles.background = "#0f0f0f";
+      gsap.to($headerContent, {
+        position: 'fixed',
+        top: 0,
+        background: '#0f0f0f'
+      });
     }
     else {
-      styles.position = "static";
-      styles.background = "unset";
+      gsap.to($headerContent, {
+        position: 'static',
+        background: 'transparent'
+      });
     }
   }
 
@@ -59,31 +92,28 @@ import services from "./data/services.js";
     $primaryNav.classList.toggle('expand');
   }
 
-  portfolio.forEach(info => {
-    $portfolioContent.innerHTML += `
-      <a href=${info.projectUrl} class="card bg-surface relative flow" style="--flow-space: 1rem;text-decoration:none;color: inherit">
-        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="41" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layout"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg>
-        <h3 class="card__title font-heading uppercase">${info.title}</h3>
-        <p class="text-sm">${info.content}</p>
-      </a>`;
-  });
+  function renderSection($section, infos) {
+    let clutter = '';
+    infos.forEach(info => {
+      clutter += `
+        <article class="card bg-surface relative flow" style="--flow-space: 1rem">
+          <img src=${info.imgUrl} height="41px" alt=${info.title}>
+          <h3 class="card__title font-heading uppercase">${info.title}</h3>
+          <p class="text-sm">${info.content}</p>
+          ${info.projectUrl ? `<a href=${info.projectUrl} class="absolute inset-0"></a>` : ''}
+        </article>`;
+    });
+    $section.innerHTML = clutter;
+  }
 
-  services.forEach(info => {
-    $servicesContent.innerHTML += `
-      <div class="card bg-surface flow" style="--flow-space: 1rem">
-        <img src=${info.imgUrl} height="41px" alt="...">
-        <h3 class="card__title font-heading uppercase">${info.title}</h3>
-        <p class="text-sm">${info.content}</p>
-      </div>`;
-  })
-
-  function animateHeadline(words, $text) {
+  function animateHeadline(words, $textElem) {
+    $textElem.classList.add('animate-rotate');
     let count = 0;
     setInterval(() => {
       if (count === words.length) {
         count = 0;
       }
-      $text.textContent = words[count++];
+      $textElem.textContent = words[count++];
     }, 2000);
   }
 
